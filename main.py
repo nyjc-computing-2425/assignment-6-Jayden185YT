b204bfb33e77a2cdd34cfe10829594357ec6b039
Jayden Tiew <106733343+Jayden185YT@users.noreply.github.com>
2024-03-28 07:27:16 +0000

    Final Submission

--- main.py
+++ main.py
@@ -4,17 +4,46 @@ import time
 
 # Part 1
 def clock(n):
-    # Your code here
-    pass
-
+    """
+    Show the time and update it once for n seconds
+    """
+    for i in range(n):
+        t = datetime.now()
+        print(t.strftime("%X"), end="\r")
+        time.sleep(1)
+        
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+    """
+    Returns the lowest common multiple of a and b
+    a : int
+    The first number
+    b : int
+    The second number
+    """
+    a1 , b1 = a, b
+    while a != b:
+        if a < b:
+            a += a1
+        else:
+            b += b1
+    return a
+        
 
 
 # Part 3
 def gcf(a, b):
-    # Your code here
-    pass
+    """
+    return the greatest common factor of a and b
+    a : int
+    The first number
+    b : int
+    The second number
+    """
+    while b != 0:
+        r = a%b
+        a = b
+        b = r
+    return a
+
